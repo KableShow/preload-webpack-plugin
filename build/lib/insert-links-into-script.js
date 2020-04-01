e5903c0a4bc9e648b21c4851620ef7b84f2da7e2
zhengjinbao <zhengjinbao@bluemoon.com.cn>
2020-04-01 14:45:23 +0800

    低级bug

--- build/lib/insert-links-into-script.js
+++ build/lib/insert-links-into-script.js
@@ -8,7 +8,7 @@ function insertLinksIntoScript({
     return html;
   }
 
-  var insertScriptText = '(function(){window.onload=function(){var b=[xxxxx];var a=document.createDocumentFragment();b.forEach(function(c){var d=document.createElement(\"link\");d.setAttribute(\"rel\",\"prefetch\");d.setAttribute(\"href\",c);a.apprendChild(d)});document.body.appendChild(a)}})();';
+  var insertScriptText = '(function(){window.onload=function(){var b=[xxxxx];var a=document.createDocumentFragment();b.forEach(function(c){var d=document.createElement(\"link\");d.setAttribute(\"rel\",\"prefetch\");d.setAttribute(\"href\",c);a.appendChild(d)});document.body.appendChild(a)}})();';
 
   if (html.includes('</body>')) {
     const hrefStr = hrefs.reduce((t, href) => {
